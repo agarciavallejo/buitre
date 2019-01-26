935a20fcafcae0113d54150b6be2723224e12ec8
adrl102 <a.garcia.vallejo@gmail.com>
2019-01-26 15:09:48 +0100

    user

--- Backend/src/entities/user.py
+++ Backend/src/entities/user.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, String, Numeric, Integer, Boolean
 from .entity import Entity, Base
+from marshmallow import Schema, fields
 
 class User(Entity, Base):
     __tablename__ = 'User'
@@ -11,3 +12,17 @@ class User(Entity, Base):
     radius = Column(Integer)
     is_valid = Column(Boolean)
     score = Column(Integer)
+    
+    def __init__(self, name, created_by):
+        Entity.__init__(self, created_by)
+        self.name = name
+
+class UserSchema(Schema):
+    id = fields.Integer()
+    name = fields.Str()
+    email = fields.Str()
+    latitude = fields.Decimal()
+    longitude = fields.Decimal()
+    radius = fields.Integer()
+    is_valid = fields.Boolean()
+    score = fields.Integer()
--- Backend/src/main.py
+++ Backend/src/main.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from config import Config
-
+from .entities.user import User, UserSchema
+from .entities.entity import Base
 
 #create app
 app = Flask(__name__)
@@ -13,9 +14,25 @@ CORS(app)
 app.config.from_object(Config)
 engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 Session = sessionmaker(bind=engine)
-
 Base.metadata.create_all(engine)
 
-@app.route("/")
-def hello():
-    return 'hello world'
+session = Session()
+
+#create test user
+initial_users = session.query(User).all()
+if len(initial_users) == 0:
+    test_user = User("test", "script")
+    test_user.is_valid = True;
+    session.add(test_user)
+    session.commit()
+    session.close()
+
+#public methods
+@app.route("/users")
+def get_users():
+    users_object = session.query(User).all()
+    user_schema = UserSchema(many = True)
+    users = user_schema.dump(users_object)
+    session.close()
+    return jsonify(users)
+    
