7672c8cc873dd3859428e53ab83de7ee2d25967d
andreuramos <andreu.ramos.amengual@gmail.com>
2019-02-05 20:47:40 +0100

    CS FIXER: test2

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -8,42 +8,43 @@ from .entities.oportunity import Oportunity, OportunitySchema
 from .entities.entity import Base
 from controller import Controller
 
-# Comment added to test autopep hook
+# Comment added to test autopep hook  adas
 
-#create app
+# create app
 app = Flask(__name__)
 CORS(app)
 
-#database
+# database
 app.config.from_object(Config)
 engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 Session = sessionmaker(bind=engine)
 Base.metadata.create_all(engine)
-             
+
 session = Session()
 
-#create test user
+# create test user
 initial_users = session.query(User).all()
 if len(initial_users) == 0:
     test_user = User("test2", "script")
-    test_user.is_valid = True;
+    test_user.is_valid = True
     session.add(test_user)
     session.commit()
     session.close()
 
-#public methods
+# public methods
 @app.route("/users")
 def get_users():
     users_object = session.query(User).all()
-    user_schema = UserSchema(many = True)
+    user_schema = UserSchema(many=True)
     users = user_schema.dump(users_object)
     session.close()
     return jsonify(users)
 
+
 @app.route("/oportunity")
 def list_oportunities():
     db_opos = session.query(Oportunity).all()
-    o_sch = OportunitySchema(many = True)
+    o_sch = OportunitySchema(many=True)
     oportunities = o_sch.dump(db_opos)
     session.close()
     return jsonify(oportunities)
@@ -55,7 +56,7 @@ def create_opo(user_id):
     c = Controller(session)
     user = c.getUser(user_id)
     if(user):
-        c.createOportunity("Oportunitat2",user_id)
+        c.createOportunity("Oportunitat2", user_id)
         return "DONE"
     else:
         return "USER NOT FOUND"
