ce422b3935def414cdc7afd7e382eaab4675ce06
andreuramos <andreu.ramos.amengual@gmail.com>
2019-02-14 17:23:34 +0100

    ALEMBIC: reordered tables in migration in order to ensure relations can be found

--- Backend/alembic/versions/47ef3050097f_initial_schema.py
+++ Backend/alembic/versions/47ef3050097f_initial_schema.py
@@ -18,136 +18,136 @@ depends_on = None
 
 def upgrade():
     op.create_table(
-    	'Comment',
-    	sa.Column('id', sa.Integer, primary_key=True),
-    	sa.Column('created_at', sa.DateTime),
-    	sa.Column('updated_at', sa.DateTime),
-    	sa.Column('last_updated_by', sa.String(16)),
-    	sa.Column('opportunity_id', sa.Integer, sa.ForeignKey('Opportunity.id'), nullable=False),
-    	sa.Column('user_id', sa.Integer, sa.ForeignKey('User.id'), nullable=False),
-    	sa.Column('score', sa.Numeric)  # Score of the comment
+        'User',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('created_at', sa.DateTime),
+        sa.Column('updated_at', sa.DateTime),
+        sa.Column('last_updated_by', sa.String(16)),
+        sa.Column("name", sa.String),
+        sa.Column("email", sa.String),
+        sa.Column('password', sa.String),
+        sa.Column('latitude', sa.Numeric(9, 6)),
+        sa.Column('longitude', sa.Numeric(9, 6)),
+        sa.Column('radius', sa.Integer),
+        sa.Column('is_valid', sa.Boolean),
+        sa.Column('score', sa.Integer)
     )
 
     op.create_table(
-    	'CommentLike',
-    	sa.Column('id', sa.Integer, primary_key=True),
-    	sa.Column('created_at', sa.DateTime),
-    	sa.Column('updated_at', sa.DateTime),
-    	sa.Column('last_updated_by', sa.String(16)),
-    	sa.Column('comment_id', sa.Integer, sa.ForeignKey('Comment.id'), nullable=False),
-    	sa.Column('user_id', sa.Integer, sa.ForeignKey('User.id'), nullable=False),
-    	sa.Column('score', sa.Numeric)  # Thumbs up / down ??
+        'Opportunity',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('created_at', sa.DateTime),
+        sa.Column('updated_at', sa.DateTime),
+        sa.Column('last_updated_by', sa.String(16)),
+        sa.Column("name", sa.String),
+        sa.Column("description", sa.String),
+        sa.Column("latitude", sa.Numeric(9, 6)),
+        sa.Column("longitude", sa.Numeric(9, 6)),
+        sa.Column("score", sa.Numeric),
+        sa.Column("closing_date", sa.Date),
+        sa.Column("user_id", sa.Integer, sa.ForeignKey('User.id'), nullable=False),
     )
 
     op.create_table(
-    	'Opportunity',
-    	sa.Column('id', sa.Integer, primary_key=True),
-    	sa.Column('created_at', sa.DateTime),
-    	sa.Column('updated_at', sa.DateTime),
-    	sa.Column('last_updated_by', sa.String(16)),
-    	sa.Column("name", sa.String),
-    	sa.Column("description", sa.String),
-    	sa.Column("latitude", sa.Numeric(9, 6)),
-    	sa.Column("longitude", sa.Numeric(9, 6)),
-    	sa.Column("score", sa.Numeric),
-    	sa.Column("closing_date", sa.Date),
-    	sa.Column("user_id", sa.Integer, sa.ForeignKey('User.id'), nullable=False),
+        'Tag',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('created_at', sa.DateTime),
+        sa.Column('updated_at', sa.DateTime),
+        sa.Column('last_updated_by', sa.String(16)),
+        sa.Column("name", sa.String),
+        sa.Column("tag_id", sa.Integer, sa.ForeignKey('Tag.id'))
     )
 
     op.create_table(
-    	'OpportunityLike',
-    	sa.Column('id', sa.Integer, primary_key=True),
-    	sa.Column('created_at', sa.DateTime),
-    	sa.Column('updated_at', sa.DateTime),
-    	sa.Column('last_updated_by', sa.String(16)),
-		sa.Column("opportunity_id", sa.Integer, sa.ForeignKey('Opportunity.id'), nullable=False),
-    	sa.Column("user_id", sa.Integer, sa.ForeignKey('User.id'), nullable=False),
-    	sa.Column("score", sa.Integer)  # ??
+        'Comment',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('created_at', sa.DateTime),
+        sa.Column('updated_at', sa.DateTime),
+        sa.Column('last_updated_by', sa.String(16)),
+        sa.Column('opportunity_id', sa.Integer, sa.ForeignKey('Opportunity.id'), nullable=False),
+        sa.Column('user_id', sa.Integer, sa.ForeignKey('User.id'), nullable=False),
+        sa.Column('score', sa.Numeric)  # Score of the comment
     )
 
     op.create_table(
-    	'OpportunitySchedule',
-    	sa.Column('id', sa.Integer, primary_key=True),
-    	sa.Column('created_at', sa.DateTime),
-    	sa.Column('updated_at', sa.DateTime),
-    	sa.Column('last_updated_by', sa.String(16)),
-    	sa.Column("start_time", sa.Time, nullable=False),
-    	sa.Column("end_time", sa.Time, nullable=False),
-    	sa.Column("monday", sa.Boolean),
-    	sa.Column("tuesday", sa.Boolean),
-    	sa.Column("wednesday", sa.Boolean),
-    	sa.Column("thursday", sa.Boolean),
-    	sa.Column("friday", sa.Boolean),
-    	sa.Column("saturday", sa.Boolean),
-    	sa.Column("sunday", sa.Boolean),
-    	sa.Column("opportunity_id", sa.Integer, sa.ForeignKey('Opportunity.id'), nullable=False)
+        'CommentLike',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('created_at', sa.DateTime),
+        sa.Column('updated_at', sa.DateTime),
+        sa.Column('last_updated_by', sa.String(16)),
+        sa.Column('comment_id', sa.Integer, sa.ForeignKey('Comment.id'), nullable=False),
+        sa.Column('user_id', sa.Integer, sa.ForeignKey('User.id'), nullable=False),
+        sa.Column('score', sa.Numeric)  # Thumbs up / down ??
     )
 
     op.create_table(
-    	'OpportunityTag',
-    	sa.Column('id', sa.Integer, primary_key=True),
-    	sa.Column('created_at', sa.DateTime),
-    	sa.Column('updated_at', sa.DateTime),
-    	sa.Column('last_updated_by', sa.String(16)),
-    	sa.Column("opportunity_id", sa.Integer, sa.ForeignKey('Opportunity.id'), nullable=False, primary_key=True),
-    	sa.Column("tag_id", sa.Integer, sa.ForeignKey('Tag.id'), nullable=False, primary_key=True)
+        'OpportunityLike',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('created_at', sa.DateTime),
+        sa.Column('updated_at', sa.DateTime),
+        sa.Column('last_updated_by', sa.String(16)),
+                sa.Column("opportunity_id", sa.Integer, sa.ForeignKey('Opportunity.id'), nullable=False),
+        sa.Column("user_id", sa.Integer, sa.ForeignKey('User.id'), nullable=False),
+        sa.Column("score", sa.Integer)  # ??
     )
 
     op.create_table(
-    	'Picture',
-    	sa.Column('id', sa.Integer, primary_key=True),
-    	sa.Column('created_at', sa.DateTime),
-    	sa.Column('updated_at', sa.DateTime),
-    	sa.Column('last_updated_by', sa.String(16)),
-    	sa.Column("opportunity_id", sa.Integer, sa.ForeignKey('Opportunity.id'), nullable=False),
-    	sa.Column('path', sa.String)
+        'OpportunitySchedule',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('created_at', sa.DateTime),
+        sa.Column('updated_at', sa.DateTime),
+        sa.Column('last_updated_by', sa.String(16)),
+        sa.Column("start_time", sa.Time, nullable=False),
+        sa.Column("end_time", sa.Time, nullable=False),
+        sa.Column("monday", sa.Boolean),
+        sa.Column("tuesday", sa.Boolean),
+        sa.Column("wednesday", sa.Boolean),
+        sa.Column("thursday", sa.Boolean),
+        sa.Column("friday", sa.Boolean),
+        sa.Column("saturday", sa.Boolean),
+        sa.Column("sunday", sa.Boolean),
+        sa.Column("opportunity_id", sa.Integer, sa.ForeignKey('Opportunity.id'), nullable=False)
     )
 
     op.create_table(
-    	'Tag',
-    	sa.Column('id', sa.Integer, primary_key=True),
-    	sa.Column('created_at', sa.DateTime),
-    	sa.Column('updated_at', sa.DateTime),
-    	sa.Column('last_updated_by', sa.String(16)),
-    	sa.Column("name", sa.String),
-    	sa.Column("tag_id", sa.Integer, sa.ForeignKey('Tag.id'))
+        'OpportunityTag',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('created_at', sa.DateTime),
+        sa.Column('updated_at', sa.DateTime),
+        sa.Column('last_updated_by', sa.String(16)),
+        sa.Column("opportunity_id", sa.Integer, sa.ForeignKey('Opportunity.id'), nullable=False, primary_key=True),
+        sa.Column("tag_id", sa.Integer, sa.ForeignKey('Tag.id'), nullable=False, primary_key=True)
     )
 
     op.create_table(
-    	'User',
-    	sa.Column('id', sa.Integer, primary_key=True),
-    	sa.Column('created_at', sa.DateTime),
-    	sa.Column('updated_at', sa.DateTime),
-    	sa.Column('last_updated_by', sa.String(16)),
-    	sa.Column("name", String),
-    	sa.Column("email", String),
-    	sa.Column('password', String),
-    	sa.Column('latitude', sa.Numeric(9, 6)),
-    	sa.Column('longitude', sa.Numeric(9, 6)),
-    	sa.Column('radius', sa.Integer),
-    	sa.Column('is_valid', sa.Boolean),
-    	sa.Column('score', sa.Integer)
+        'Picture',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('created_at', sa.DateTime),
+        sa.Column('updated_at', sa.DateTime),
+        sa.Column('last_updated_by', sa.String(16)),
+        sa.Column("opportunity_id", sa.Integer, sa.ForeignKey('Opportunity.id'), nullable=False),
+        sa.Column('path', sa.String)
     )
 
     op.create_table(
-    	'UserTag',
-    	sa.Column('id', sa.Integer, primary_key=True),
-    	sa.Column('created_at', sa.DateTime),
-    	sa.Column('updated_at', sa.DateTime),
-    	sa.Column('last_updated_by', sa.String(16)),
-    	sa.Column("tag_id", sa.Integer, sa.ForeignKey('Tag.id'), nullable=False),
-		sa.Column("tag_id", sa.Integer, sa.ForeignKey('Tag.id'), nullable=False)
+        'UserTag',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('created_at', sa.DateTime),
+        sa.Column('updated_at', sa.DateTime),
+        sa.Column('last_updated_by', sa.String(16)),
+        sa.Column("tag_id", sa.Integer, sa.ForeignKey('Tag.id'), nullable=False),
+                sa.Column("tag_id", sa.Integer, sa.ForeignKey('Tag.id'), nullable=False)
     )
 
 
 def downgrade():
-	op.drop_table('UserTag')
-	op.drop_table('User')
-	op.drop_table('Tag')
-	op.drop_table('Picture')
-	op.drop_table('OpportunityTag')
-	op.drop_table('OpportunitySchedule')
-	op.drop_table('OpportunityLike')
-	op.drop_table('Opportunity')
-	op.drop_table('CommentLike')
+    op.drop_table('UserTag')
+    op.drop_table('Picture')
+    op.drop_table('OpportunityTag')
+    op.drop_table('OpportunitySchedule')
+    op.drop_table('OpportunityLike')
+    op.drop_table('CommentLike')
     op.drop_table('Comment')
+    op.drop_table('Tag')
+    op.drop_table('Opportunity')
+    op.drop_table('User')
