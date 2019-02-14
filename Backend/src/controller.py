7bbbee5421137fc69ab9de8b24638ad51d749882
andreuramos <andreu.ramos.amengual@gmail.com>
2019-02-14 18:15:01 +0100

    Replaces oportunity word for opportunity everywhere

--- Backend/src/controller.py
+++ Backend/src/controller.py
@@ -1,27 +1,27 @@
 from .entities.user import User, UserSchema
-from .entities.oportunity import Oportunity, OportunitySchema
+from .entities.oportunity import Opportunity, OpportunitySchema
+
 
 class Controller:
 
     session = None
 
-    def __init__(self,session):
+    def __init__(self, session):
         self.session = session
 
-    def getUser(self,user_id):
+    def getUser(self, user_id):
         sch = UserSchema()
         db_user = self.session.query(User).get(user_id)
         if (None == db_user):
-            return None # raise UserNotFoundException
+            return None  # raise UserNotFoundException
         return sch.dump(db_user)
 
-    def getOportunity(self,id):
-        db_oportunity = self.session.query(Oportunity).get(id)
-        sch = OportunitySchema()
+    def getOpportunity(self, id):
+        db_opportunity = self.session.query(Opportunity).get(id)
+        sch = OpportunitySchema()
 
-
-    def createOportunity(self,name,user_id):
-        test_opo = Oportunity(name,user_id, description = "", latitude = 0.0, longitude = 0.0, score = 0, closing_date = None)
+    def createOpportunity(self, name, user_id):
+        test_opo = Opportunity(name, user_id, description="", latitude=0.0, longitude=0.0, score=0, closing_date=None)
         self.session.add(test_opo)
         self.session.commit()
         return True
--- Backend/src/entities/opportunityTag.py
+++ Backend/src/entities/opportunityTag.py
@@ -2,22 +2,20 @@ from sqlalchemy import Column, Integer, ForeignKey
 from .entity import Entity, Base
 from marshmallow import Schema, fields
 
-class OportunityTag(Entity, Base):
-    __tablename__ = 'OportunityTag'
 
-    oportunity_id   = Column("oportunity_id", Integer, 
-        ForeignKey('Oportunity.id'), nullable=False, primary_key=True)
-    tag_id          = Column("tag_id"       , Integer, 
-        ForeignKey('Tag.id'),        nullable=False, primary_key=True)
+class OpportunityTag(Entity, Base):
+    __tablename__ = 'OpportunityTag'
 
-    def __init__(self, oportunity_id, tag_id):
-        self.oportunity_id  = oportunity_id
-        self.tag_id         = tag_id
-        
+    opportunity_id = Column("opportunity_id", Integer,
+        ForeignKey('Opportunity.id'), nullable=False, primary_key=True)
+    tag_id = Column("tag_id", Integer,
+        ForeignKey('Tag.id'), nullable=False, primary_key=True)
 
-class OportunityTagSchema(Schema):
-    oportunity_id   = fields.Integer()
-    tag_id          = fields.Integer()
+    def __init__(self, opportunity_id, tag_id):
+        self.opportunity_id = opportunity_id
+        self.tag_id = tag_id
 
 
-        
+class OpportunityTagSchema(Schema):
+    opportunity_id = fields.Integer()
+    tag_id = fields.Integer()
