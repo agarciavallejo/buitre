c22e2af6362c50ae6e4cfe0de993effa7b1c9d53
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2019-06-14 18:51:10 +0200

    API - Refactored UserService as CreateUserService with call static method

--- app/api/user_api.py
+++ app/api/user_api.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from ..services.user.userService import UserService
+from ..services.user.createUserService import CreateUserService
 from ..libs.exceptions.emailInUseException import EmailInUseException
 
 user_api = Blueprint('user_api',__name__)
@@ -16,10 +16,9 @@ def create_user():
 		'password': password
 	}
 
-	service = UserService()
 	result = {}
 	try:
-		user = service.create(args)
+		user = CreateUserService.call(args)
 		result['success'] = True
 	except EmailInUseException as e:
 		result['error'] = e.message
