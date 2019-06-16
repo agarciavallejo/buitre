ec4b0d09b02d2957536bc64c1cad07e4225af33f
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2019-06-16 17:59:08 +0200

    API - CreateUserService tests: tested argument management

--- tests/services/test_createUserService.py
+++ tests/services/test_createUserService.py
@@ -4,9 +4,20 @@ from ...app.services.user.createUserService import CreateUserService
 from ...app.libs.exceptions import ArgumentException
 
 
-@pytest
 def test_emailless_call_raises_exception():
     with pytest.raises(ArgumentException):
         service = CreateUserService(user_repository=None, user_factory=None)
         service.call({'name': 'andreu', 'password': 'password123'})
+
+
+def test_nameless_call_raises_exception():
+    with pytest.raises(ArgumentException):
+        service = CreateUserService(user_repository=None, user_factory=None)
+        service.call({'email': 'aramos@buitre.com', 'password': 'password123'})
+
+
+def test_passwordless_call_raises_exception():
+    with pytest.raises(ArgumentException):
+        service = CreateUserService(user_repository=None, user_factory=None)
+        service.call({'email': 'aramos@buitre.com', 'name': 'andreu'})
 
