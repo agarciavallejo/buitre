803bc899289ef9128396795501febb6d52d03f57
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2019-06-16 17:55:16 +0200

    deleted old useless test

--- tests/services/test_createUserService.py
+++ tests/services/test_createUserService.py
@@ -1,6 +1,12 @@
 import pytest
-import mocker
+
+from ...app.services.user.createUserService import CreateUserService
+from ...app.libs.exceptions import ArgumentException
 
 
-def test_emailless_call_raises_exception(mocker):
-    mocker.patch.object(CreateUserService, 'call')
+@pytest
+def test_emailless_call_raises_exception():
+    with pytest.raises(ArgumentException):
+        service = CreateUserService(user_repository=None, user_factory=None)
+        service.call({'name': 'andreu', 'password': 'password123'})
+
