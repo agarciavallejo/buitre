bb0619e337619aa8534f51ef57cf82747ac3ce5e
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2019-06-16 17:00:13 +0200

    API - Create User Service: PEP8

--- app/services/user/createUserService.py
+++ app/services/user/createUserService.py
@@ -2,23 +2,24 @@ from ...entities.user import User
 from ...libs.exceptions import ArgumentException, EmailInUseException
 from werkzeug.security import generate_password_hash
 
+
 class CreateUserService:
 
 	@staticmethod
 	def call(args):
-		print(args)
-		if ('email' not in args or args['email'] is None):
+
+		if 'email' not in args or args['email'] is None:
 			raise ArgumentException('email')
-		if ('name' not in args or args['name'] is None):
+		if 'name' not in args or args['name'] is None:
 			raise ArgumentException('name')
-		if ('password' not in args or args['password'] is None):
+		if 'password' not in args or args['password'] is None:
 			raise ArgumentException('password')
 
 		name = args['name']
 		email = args['email']
 		raw_password = args['password']
 
-		if (User.getByEmail(email) is not None):
+		if User.getByEmail(email) is not None:
 			raise EmailInUseException()
 
 		hashed_password = generate_password_hash(raw_password)
