101cb4f64ddcb35a01e233778b77b5f4900d6388
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2019-06-17 18:10:58 +0200

    API - Create User Service: PEP8 indentation with spaces

--- app/services/user/createUserService.py
+++ app/services/user/createUserService.py
@@ -4,27 +4,27 @@ from werkzeug.security import generate_password_hash
 
 class CreateUserService:
 
-	def __init__(self, user_repository, user_factory):
-		self.userRepository = user_repository
-		self.userFactory = user_factory
+    def __init__(self, user_repository, user_factory):
+        self.userRepository = user_repository
+        self.userFactory = user_factory
 
-	def call(self, args):
+    def call(self, args):
 
-		if 'email' not in args or args['email'] is None:
-			raise ArgumentException('email')
-		if 'name' not in args or args['name'] is None:
-			raise ArgumentException('name')
-		if 'password' not in args or args['password'] is None:
-			raise ArgumentException('password')
+        if 'email' not in args or args['email'] is None:
+            raise ArgumentException('email')
+        if 'name' not in args or args['name'] is None:
+            raise ArgumentException('name')
+        if 'password' not in args or args['password'] is None:
+            raise ArgumentException('password')
 
-		name = args['name']
-		email = args['email']
-		raw_password = args['password']
+        name = args['name']
+        email = args['email']
+        raw_password = args['password']
 
-		if self.userRepository.get_by_email(email) is not None:
-			raise EmailInUseException()
+        if self.userRepository.get_by_email(email) is not None:
+            raise EmailInUseException()
 
-		hashed_password = generate_password_hash(raw_password)
+        hashed_password = generate_password_hash(raw_password)
 
-		user = self.userFactory.create(name, email, hashed_password)
-		self.userRepository.persist(user)
+        user = self.userFactory.create(name, email, hashed_password)
+        self.userRepository.persist(user)
