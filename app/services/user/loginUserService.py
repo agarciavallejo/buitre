588a26b593804295bfe23b5b81152c6a74bfd399
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2019-06-22 10:46:51 +0200

    API - LoginUserService: test wrong password and user not found

--- app/services/user/loginUserService.py
+++ app/services/user/loginUserService.py
@@ -1,23 +1,28 @@
 from ...libs.exceptions import AuthenticationException, ArgumentException, NoValidUserException
 from werkzeug.security import check_password_hash
 
+
 class LoginUserService:
 
-	@staticmethod
-	def call(args):
-		if ('email' not in args or args['email'] is None):
-			raise ArgumentException('email')
-		if ('password' not in args or args['password'] is None):
-			raise ArgumentException('password')
+    def __init__(self, user_repository, token_generator, hash_checker_func):
+        self.user_repository = user_repository
+        self.token_generator = token_generator
+        self.hash_checker_func = hash_checker_func
 
-		email = args['email']
-		password = args['password']
+    def call(self, args):
+        if 'email' not in args or args['email'] is None:
+            raise ArgumentException('email')
+        if 'password' not in args or args['password'] is None:
+            raise ArgumentException('password')
 
-		user = User.getByEmail(email)
+        email = args['email']
+        password = args['password']
 
-		if(user is None or not check_password_hash(user.password,password)):
-			raise AuthenticationException()
-		if(not user.is_valid):
-			raise NoValidUserException()
+        user = self.user_repository.get_by_email(email)
 
-		return 'this-is-a-session-token'
+        if user is None or not self.hash_checker_func(user.password, password):
+            raise AuthenticationException()
+        if not user.is_valid:
+            raise NoValidUserException()
+
+        return 'this-is-a-session-token'
