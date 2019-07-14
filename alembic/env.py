c9564a1487778efb803924c4830c4b69d2dfde2e
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2019-07-14 20:23:01 +0200

    alembic configured online migrations with environ db url too

--- alembic/env.py
+++ alembic/env.py
@@ -2,8 +2,8 @@ from __future__ import with_statement
 
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import create_engine
+# from sqlalchemy import pool
 
 from alembic import context
 import os
@@ -57,11 +57,14 @@ def run_migrations_online():
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
+    connectable = create_engine(os.environ.get('DATABASE_URL'))
+    """    
+        engine_from_config(
         config.get_section(config.config_ini_section),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
+    """
 
     with connectable.connect() as connection:
         context.configure(
