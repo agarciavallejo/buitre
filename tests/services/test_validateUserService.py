973f02dde246a7a75a79e0d7d2af4460b445caa4
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2019-08-01 19:28:14 +0200

    create, login and validate tests reimplemented with fixtures

--- tests/services/test_validateUserService.py
+++ tests/services/test_validateUserService.py
@@ -5,80 +5,61 @@ from ...app.services.user.validateUserService import ValidateUserService
 from ...app.entities.user import User
 
 
-def test_missing_user_id():
-    with pytest.raises(ArgumentException):
-        service = ValidateUserService(
-            user_repository=None,
-            validation_token_verifier=None
-        )
+class fakeRepo:
+    @staticmethod
+    def get_by_email(email):
+        if email != "email@email.com":
+            return None
+        user = User("andreu", email, "123")
+        user.id = id
+        user.is_valid = False
+        return user
 
-        service.call({})
-
-
-def test_invalid_token():
-    with pytest.raises(UserValidationException):
-        def dummyverifier(token):
-            raise InvalidTokenException()
-
-        service = ValidateUserService(
-            validation_token_verifier=dummyverifier,
-            user_repository=None
-        )
-
-        service.call({'validation_token': "invalid_token"})
+    @staticmethod
+    def persist(user):
+        return user
 
 
-def test_expired_token():
-    with pytest.raises(UserValidationException):
-        def dummyverifier(token):
-            raise ExpiredTokenException()
-
-        service = ValidateUserService(
-            validation_token_verifier=dummyverifier,
-            user_repository=None
-        )
-
-        service.call({'validation_token': "expired_token"})
+def dummy_verifier(token):
+    if token == "invalid_token":
+        raise InvalidTokenException
+    if token == "expired_token":
+        raise ExpiredTokenException
+    if token == "unexisting_user":
+        return False
+    return "email@email.com"
 
 
-def test_unexisting_user():
-    with pytest.raises(UserValidationException):
-        class fakeRepo:
-            @staticmethod
-            def get_by_email(email):
-                None
-
-        def dummyverifier(token):
-            return "email@email.com"
-
-        service = ValidateUserService(
-            user_repository=fakeRepo,
-            validation_token_verifier=dummyverifier
-        )
-
-        service.call({'validation_token': "something"})
-
-
-def test_user_is_validated():
-    class fakeRepo:
-        @staticmethod
-        def get_by_email(email):
-            user = User("andreu", email, "123")
-            user.id = id
-            user.is_valid = False
-            return user
-
-        @staticmethod
-        def persist(user):
-            return user
-
-    def dummy_verifier(token):
-        return True
-
+@pytest.fixture(scope="module")
+def service():
     service = ValidateUserService(
         user_repository=fakeRepo,
         validation_token_verifier=dummy_verifier
     )
-    user = service.call({'validation_token': "sometoken"})
+    return service
 
+
+# TEST CASES
+def test_missing_user_id(service):
+    with pytest.raises(ArgumentException):
+        service.call({})
+
+
+def test_invalid_token(service):
+    with pytest.raises(UserValidationException):
+        service.call({'validation_token': "invalid_token"})
+
+
+def test_expired_token(service):
+    with pytest.raises(UserValidationException):
+        service.call({'validation_token': "expired_token"})
+
+
+def test_unexisting_user(service):
+    with pytest.raises(UserValidationException):
+        service.call({'validation_token': "unexisting_user"})
+
+
+def test_user_is_validated(service):
+    user = service.call({'validation_token': "sometoken"})
     assert user.is_valid
