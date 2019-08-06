6bb358c79b40cf76e8f3bb56eba2c882b3646ba5
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2019-08-06 23:31:56 +0200

    corrected GetProfileService call

--- app/api/profile_api.py
+++ app/api/profile_api.py
@@ -1,26 +1,28 @@
-from flask import g, jsonify
+from flask import g, jsonify, Blueprint
 
 from ..api import authenticate_user, GetProfileService
 from ..routes import app
 
-
-@app.route('/<int:id>', methods=['GET'])
-def get_public_profile(id):
-    pass
+profile_api = Blueprint('profile_api', __name__)
 
 
-@app.route('/', methods=["GET"])
+@profile_api.route('/', methods=["GET"])
 @authenticate_user
 def get_profile():
     user_id = g.user_id
 
-    response = GetProfileService({'user_id': user_id})
+    response = GetProfileService.call({'user_id': user_id})
     response_code = 200
 
     return jsonify(response), response_code
 
 
-@app.route('/update', methods=["POST"])
+@profile_api.route('/<int:id>', methods=['GET'])
+def get_public_profile(id):
+    pass
+
+
+@profile_api.route('/update', methods=["POST"])
 @authenticate_user
 def update_profile():
     pass
