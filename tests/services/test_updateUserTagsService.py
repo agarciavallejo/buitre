04b596d140d4a8c13563189914dc28a045cbc92c
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2019-08-10 13:37:22 +0200

    Update Tags service created & tested

--- tests/services/test_updateUserTagsService.py
+++ tests/services/test_updateUserTagsService.py
@@ -1,13 +1,55 @@
 import pytest
 
+from ...app.entities.tag import Tag
 from ...app.utils.exceptions import ArgumentException
-from ...app.services.profile.updateUserTags import UpdateUserTags
+from ...app.services.profile.updateUserTagsService import UpdateUserTagsService
+
+
+class FakeTagRepository:
+    @staticmethod
+    def get_by_name(tag_name):
+        if tag_name == "Bicicletas":
+            return Tag("Bicicletas")
+
+    @staticmethod
+    def remove_from_user(user_id):
+        pass
+
+    @staticmethod
+    def add_to_user(user_id, tag):
+        pass
+
+    @staticmethod
+    def get_by_user_id(user_id):
+        if user_id == 1:
+            return [Tag("Bicicletas")]
+
 
 @pytest.fixture
 def service():
-    service = UpdateUserTags()
+    service = UpdateUserTagsService(
+        tag_repository=FakeTagRepository
+    )
     return service
 
-def test_arguments(service):
+
+def test_no_arguments(service):
     with pytest.raises(ArgumentException):
         service.call({})
+
+
+def test_no_tags_argument(service):
+    with pytest.raises(ArgumentException):
+        service.call({'user_id': 1})
+
+
+def test_emtpy_tags_argument(service):
+    with pytest.raises(ArgumentException):
+        service.call({'user_id': 1, 'tags': []})
+
+
+def test_tag_added(service):
+    tags = ["Bicicletas"]
+    user_tags = service.call({'user_id': 1, 'tags': tags})
+    assert len(user_tags) == 1
+
