c3c69d313aee650956046e7a3559d82930cd5a72
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2019-08-11 16:21:51 +0200

    Created base opportunity creation service

---
+++ app/services/opportunity/createOpportunityService.py
@@ -0,0 +1,43 @@
+from ...utils.exceptions import ArgumentException
+
+
+class CreateOpportunityService:
+
+    def __init__(self, opportunity_factory, opportunity_repository, picture_factory, picture_repository):
+        self.opportunityFactory = opportunity_factory
+        self.opportunityRepository = opportunity_repository
+        self.pictureFactory = picture_factory
+        self.pictureRepository = picture_repository
+
+    def call(self, args):
+        if 'user_id' not in args or args['user_id'] is None:
+            raise ArgumentException('user_id parameter is mandatory')
+        if 'name' not in args or args['name'] is None:
+            raise ArgumentException('name parameter is mandatory')
+        if 'pictures' not in args or not args['pictures']:
+            raise ArgumentException('pictures parameter is mandatory and should not be empty')
+        if 'latitude' not in args or not args['latitude']:
+            raise ArgumentException('latitude parameter is mandatory')
+        if 'longitude' not in args or not args['longitude']:
+            raise ArgumentException('longitude parameter is mandatory')
+
+        user_id = args['user_id']
+        name = args['name']
+        pictures = args['pictures']
+        latitude = args['latitude']
+        longitude = args['longitude']
+
+        description = args['description'] if 'description' in args else None
+        closing_date = args['closing_dat'] if 'closing_date' in args else None
+        schedules = args['schedules'] if 'schedules' in args else None
+        tags = args['tags'] if 'tags' in args else None
+
+        opportunity = self.opportunityFactory.create(user_id, name, description, latitude, longitude, closing_date)
+        opportunity = self.opportunityRepository.persist(opportunity)
+
+        for path in pictures:
+            picture = self.pictureFactory.create_for_opportunity(opportunity.id, path, user_id)
+            self.pictureRepository.persist(picture)
+            
+
+        return opportunity
---
+++ tests/services/test_createOpportunityService.py
@@ -0,0 +1,92 @@
+import pytest
+
+from ...app.entities.opportunity import Opportunity
+from ...app.entities.picture import Picture
+from ...app.services.opportunity.createOpportunityService import CreateOpportunityService
+from ...app.utils.exceptions import ArgumentException
+
+
+class FakeOpportunityFactory:
+    @staticmethod
+    def create(user_id, name, description, latitude, longitude, closing_date):
+        oppo = Opportunity(
+            name=name,
+            user_id=user_id,
+            description=description,
+            latitude=latitude,
+            longitude=longitude,
+            closing_date=closing_date
+        )
+        return oppo
+
+
+class FakeOpportunityRepository:
+    @staticmethod
+    def persist(opportunity):
+        return opportunity
+
+
+class FakePictureFactory:
+    @staticmethod
+    def create_for_opportunity(opportunity_id, path, user_id):
+        return Picture(opportunity_id, path, user_id)
+
+
+class FakePictureRepository:
+    @staticmethod
+    def persist(picture):
+        return picture
+
+
+@pytest.fixture()
+def service():
+    return CreateOpportunityService(
+        opportunity_factory=FakeOpportunityFactory,
+        opportunity_repository=FakeOpportunityRepository,
+        picture_factory=FakePictureFactory,
+        picture_repository=FakePictureRepository
+    )
+
+
+def test_arguments_user_id(service):
+    with pytest.raises(ArgumentException):
+        service.call({'name': "Test Oppo", 'pictures': ['some-path-to-a-picture'], 'latitude': 39, 'longitude': 2})
+
+
+def test_arguments_name(service):
+    with pytest.raises(ArgumentException):
+        service.call({'user_id': 1, 'pictures': ['some-path-to-a-picture'], 'latitude': 39, 'longitude': 2})
+
+
+def test_arguments_pictures(service):
+    with pytest.raises(ArgumentException):
+        service.call({'user_id': 1, 'name': "Testing Oppo", 'latitude': 39, 'longitude': 2})
+
+
+def test_arguments_pictures_empty(service):
+    with pytest.raises(ArgumentException):
+        service.call({'user_id': 1, 'name': "Testing Oppo", 'pictures': [], 'latitude': 39, 'longitude': 2})
+
+
+def test_arguments_latitude(service):
+    with pytest.raises(ArgumentException):
+        service.call({'user_id': 1, 'name': "Test Oppo", 'pictures': ['some-path-to-a-picture'], 'longitude': 2})
+
+
+def test_arguments_longitude(service):
+    with pytest.raises(ArgumentException):
+        service.call({'user_id': 1, 'name': "Test Oppo", 'pictures': ['some-path-to-a-picture'], 'latitude': 39})
+
+
+def test_opportunity_is_returned(service):
+    oppo = service.call({
+        'user_id': 1,
+        'name': "Testing Oppo",
+        'pictures': ['https://cdn.buitre.com/opomain_picture.png'],
+        'latitude': 39,
+        'longitude': 2,
+    })
+    assert oppo.name == "Testing Oppo"
+    assert oppo.user_id == 1
+    assert oppo.longitude == 2
+    assert oppo.latitude == 39
