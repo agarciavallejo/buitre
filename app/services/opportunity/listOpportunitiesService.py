e26769b1c22f0310e37fa14f8954d8dead436075
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2019-09-21 19:42:59 +0200

    search engine: basic service structure tested

--- app/services/opportunity/listOpportunitiesService.py
+++ app/services/opportunity/listOpportunitiesService.py
@@ -1,20 +1,40 @@
+from ...utils.exceptions import ArgumentException
+
+
 class ListOpportunitiesService:
 
     def __init__(self, opportunityRepository):
         self.opportunityRepository = opportunityRepository
+        self.mandatory_params = ['lat', 'lng', 'keywords', 'filters']
 
     def call(self, args):
+        for param in self.mandatory_params:
+            if param not in args:
+                raise ArgumentException('missing "' + param + '" parameter')
 
-        query = args['query']
+        lat = args['lat']
+        lng = args['lng']
+        keywords = args['keywords']
+        filters = args['filters']
 
-        db_opportunities = self.opportunityRepository.search(lat, lng, query)
+        self.opportunityRepository.initQuery()
+        if lat is not None and lng is not None:
+            self.opportunityRepository.centerQuery(lat, lng)
+
+        if keywords:
+            self.opportunityRepository.addKeyworkdsToQuery(keywords)
+
+        if len(filters):
+            for filter, value in filters:
+                self.opportunityRepository.addFilterToQuery(filter, value)
+
+        db_opportunities = self.opportunityRepository.executeQuery()
 
         results = []
         for db_opportunity in db_opportunities:
             opportunity = {
                 'id': db_opportunity.id,
                 'name': db_opportunity.name,
-
             }
             results.append(opportunity)
 
--- tests/services/test_listOpportunities.py
+++ tests/services/test_listOpportunities.py
@@ -1,15 +1,31 @@
 import pytest
 
+from ...app.entities.opportunity import Opportunity
 from ...app.utils.exceptions import ArgumentException
 from ...app.services.opportunity.listOpportunitiesService import ListOpportunitiesService
 
+# @TODO: this should be a database test (with a fake databse, maybe a unittest one)
+# https://medium.com/@vittorio.camisa/agile-database-integration-tests-with-python-sqlalchemy-and-factory-boy-6824e8fe33a1
 
 class FakeOpportunityRepo:
     def __init__(self):
-        pass
-
-    def searchByLatLng(self, lat, lng):
-        return []
+        self.lat = None
+        self.lng = None
+        self.keywords = None
+        self.filters = []
+    def initQuery(self):
+        self.__init__()
+    def centerQuery(self, lat, lng):
+        self.lat = lat
+        self.lng = lng
+    def executeQuery(self):
+        if self.lat is None and self.lng is None and self.keywords is None and len(self.filters) == 0:
+            return []
+        if self.lat is not None and self.lng is not None:
+            return [
+                Opportunity("opo1",1),
+                Opportunity("opo2",2)
+            ]
 
 
 @pytest.fixture()
@@ -19,6 +35,30 @@ def service():
     )
 
 
-def test_searchbylatlng(service):
-    results = service.call(5.2333, 2.4444)
-    assert results.length == 0
+def test_no_arguments(service):
+    with pytest.raises(ArgumentException):
+        service.call({})
+
+
+def test_arguments_none(service):
+    args = {
+        'lat': None,
+        'lng': None,
+        'keywords': None,
+        'filters': []
+    }
+    results = service.call(args)
+    assert len(results) == 0
+
+def test_latlngsearch(service):
+    args = {
+        'lat': 5.25,
+        'lng': 2.29,
+        'keywords': None,
+        'filters': []
+    }
+    results = service.call(args)
+    assert service.opportunityRepository.lat == 5.25
+    assert service.opportunityRepository.lng == 2.29
+    assert results[0]['name'] == 'opo1'
+    assert results[1]['name'] == 'opo2'
